0475a6775dd3e5498f22beae1fa77a7de072bd52
RiwiHow <53964574+RiwiHow@users.noreply.github.com>
2025-08-28 16:41:41 +0800

    refactor: performance optimization

--- src/Hooks/useSelectedText.tsx
+++ src/Hooks/useSelectedText.tsx
@@ -1,27 +1,23 @@
 import { getSelectedText } from "@raycast/api";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import processError from "../Functions/processError";
 
 export function useSelectedText() {
   const [selectedText, setSelectedText] = useState("");
   const [selectedTextError, setSelectedTextError] = useState("");
-  const textRef = useRef(selectedText); // To fix the issue where selectedText cannot be accurately retrieved in developer mode
-  textRef.current = selectedText;
 
   useEffect(() => {
     async function getSelectedTextFunc() {
       try {
         const text = await getSelectedText();
-        if (!textRef.current) {
-          setSelectedText(text ?? "");
-        }
+        setSelectedText(text ?? "");
       } catch (error) {
         processError(error, setSelectedTextError);
       }
     }
 
     getSelectedTextFunc();
-  });
+  }, []);
 
   return { selectedText, selectedTextError };
 }
