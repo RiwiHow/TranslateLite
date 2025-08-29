adfbe0fc1d4acbe562ccd52eded715eefc6f541e
RiwiHow <53964574+RiwiHow@users.noreply.github.com>
2025-08-29 16:03:06 +0800

    chore: keep `useRef` hook for development

--- src/Hooks/useSelectedText.tsx
+++ src/Hooks/useSelectedText.tsx
@@ -5,12 +5,16 @@ import processError from "../Functions/processError";
 export function useSelectedText() {
   const [selectedText, setSelectedText] = useState("");
   const [selectedTextError, setSelectedTextError] = useState("");
+  // const textRef = useRef(selectedText); // To fix the issue where selectedText cannot be accurately retrieved in developer mode when "Use Node production environment" is disabled.
+  // textRef.current = selectedText;
 
   useEffect(() => {
     async function getSelectedTextFunc() {
       try {
         const text = await getSelectedText();
+        // if (!textRef.current) {
         setSelectedText(text ?? "");
+        // }
       } catch (error) {
         processError(error, setSelectedTextError);
       }
